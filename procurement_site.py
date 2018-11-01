443791454e5fef4f49353cb6a521d060053aefae
Apoorva <apoorva19.am@gmail.com>
2018-11-01 17:04:41 +0530

    Job list displayed for Harvest Aider

--- procurement_site.py
+++ procurement_site.py
@@ -1,12 +1,11 @@
 import os
 import json
+import datetime
+from copy import deepcopy
 from flask import send_from_directory
 from flask import Flask, render_template, url_for, redirect
 from flask_migrate import Migrate
-
-from models import app, db
-from forms import  AddStalkCollector
-
+from models import app, db, Job_List
 from state_district import code, state
 
 Migrate(app, db)
@@ -23,20 +22,19 @@ def base():
 def login():
     return render_template('login.html')
 
-@app.route('/harvest-aider')
+@app.route('/harvest_aider')
 def harvest_aider():
     return render_template('/harvest_aider/index.html')
     
-@app.route('/harvest-aider/add-collector/')
+@app.route('/harvest_aider/add_collector/')
 def add_collector():
-    form = AddStalkCollector()
     district = code.get('AP')
     ind = list(range(1, len(district)+1))
-    form.district_name.choices = list(zip(ind, district))
+    district_name = list(zip(ind, district))
 
-    return render_template('/harvest_aider/add_collector.html', form=form)
+    return render_template('/harvest_aider/add_collector.html', state=list(zip(state.values(), state.keys())), district_name=district_name)
     
-@app.route('/harvest-aider/add-collector/<state>')
+@app.route('/harvest_aider/add_collector/<state>')
 def city(state):
     print(state)
     district = code.get(state)
@@ -53,6 +51,10 @@ def city(state):
 
     return json.dumps({'cities' : cityArray})
 
-    
+@app.route('/harvest_aider/job_schedule/')    
+def show_job_schedule():
+    jobs = Job_List.query.filter((Job_List.collector_id != 'Not assigned') | (Job_List.job_complete == 0))
+    return render_template('/harvest_aider/job_schedule.html', joblist=jobs)
+
 if __name__ == '__main__':
     app.run(debug=True)
